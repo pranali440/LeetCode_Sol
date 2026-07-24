aabd264d959eb8c0c0618d8d8537494a204d1ee0
Pranali Pathare <pranalipathare65@gmail.com>
2026-07-24 20:48:59 +0530

    Time: 1 ms (96.26%), Space: 43.1 MB (63.85%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,25 @@
+class Solution {
+    //solutions
+    public String mergeAlternately(String word1, String word2) {
+        
+        StringBuilder str=new StringBuilder();
+int i =0;
+while(i < word1.length() && i < word2.length())
+{
+   str.append(word1.charAt(i));
+   str.append(word2.charAt(i));
+i++;
+}
+while(i<word1.length())
+{
+ str.append(word1.charAt(i));
+ i++;
+}
+while(i<word2.length())
+{
+ str.append(word2.charAt(i));
+ i++;
+}
+return str.toString();
+    } 
+}
