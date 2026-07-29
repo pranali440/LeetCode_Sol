7eaf47c52dffa2504c4540aad04b2dd06d85b19b
Pranali Pathare <pranalipathare65@gmail.com>
2026-07-29 22:30:04 +0530

    Time: 2 ms (99.33%), Space: 44.1 MB (92.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
      
-    s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+   /* s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
        int left=0;
       int right=s.length()-1;
        while(left<right)
@@ -14,5 +14,26 @@ class Solution {
         right--;
        }
  return true;
+ */
+ // second approach where space will be O(1)
+
+  int left=0;
+      int right=s.length()-1;
+       while(left<right)
+       {
+        while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+            left++;
+        }
+          while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+            right--;
+        }
+         if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right)))
+        {
+            return false;
+        }
+        left++;
+        right--;
+       }
+        return true;
     }
 }
