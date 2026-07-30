f52aa12012186f4efc6d96e88e7ea5a32efc4ebe
Pranali Pathare <pranalipathare65@gmail.com>
2026-07-30 11:06:12 +0530

    Time: 4 ms (99.03%), Space: 47.9 MB (11.92%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,31 +1,37 @@
 class Solution {
-    public boolean validPalindrome(String s) {
+    public boolean validPalindrome(String s) 
+    {
 
-        int left = 0;
-        int right = s.length()-1;
+        int start = 0;
+        int end = s.length()-1;
 
-        while(left<right) {
+        while(start<end)
+       {
 
-            if (s.charAt(left)!= s.charAt(right)) 
+        if(s.charAt(start) != s.charAt(end))
+         {
+           return isPalindrome(s,start+1,end) || isPalindrome(s,start,end-1);
+          }
+            start++;
+            end--;
+        }
+         return true;
+    }
+
+            private boolean isPalindrome(String s,int start,int end)
             {
-    return isPalindrome(s, left+1, right) || isPalindrome(s, left, right-1);
-      }
-            left++;
-            right--;
-        }
-        return true;
-    }
-
-    private boolean isPalindrome(String s,int left,int right) {
-
-        while(left<right) {
-
-    if (s.charAt(left) != s.charAt(right)) {
-                return false;
+                while(start<end)
+                {
+                    if(s.charAt(start)!=s.charAt(end))
+                    {
+                        return false;
+                    }
+                    start++;
+                    end--;
+                  
+                }
+                  return true;
+                
             }
-            left++;
-            right--;
-        }
-        return true;
-    }
+    
 }
